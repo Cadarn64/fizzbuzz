ebfd12687725c48661c527014f29f4990346c8f6
Duncan DuncanJudy Judy <DuncanJudy@Duncans-iMac.local>
2016-03-14 15:01:45 +0000

    fizzbuzz created with ability to find fizz

---
+++ fizzbuzz.rb
@@ -0,0 +1,21 @@
+class Fizzbuzz
+
+	def is_divisable_by_three(number)
+		number % 3 == 0
+	end
+
+    def is_divisable_by_five(number)
+    	number % 5 == 0
+    end
+
+    def is_divisable_by_three_and_five(number)
+    	is_divisable_by_three(number)== true && is_divisable_by_five(number) == true
+    end
+   
+    def find_answer(number)
+    	if is_divisable_by_three(number)
+    		"fizz"
+    	end
+    end
+end
+
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,48 @@
+require "rspec"
+require "./fizzbuzz"
+
+describe "Fizzbuzz" do 
+
+	it "should know when a number is divisable by three" do 
+		fizzbuzz = Fizzbuzz.new
+		expect(fizzbuzz.is_divisable_by_three(3)).to eq true
+	end
+
+	it "should know when a number is not divisable by three" do
+		fizzbuzz = Fizzbuzz.new
+		expect(fizzbuzz.is_divisable_by_three(5)).to eq false
+	end
+
+    it "should know when a number is divisable by five" do
+    	fizzbuzz = Fizzbuzz.new
+    	expect(fizzbuzz.is_divisable_by_five(5)).to eq true 
+    end
+
+    it "should know when a number is not divisable by five" do
+    	fizzbuzz = Fizzbuzz.new
+    	expect(fizzbuzz.is_divisable_by_five(9)).to eq false
+    end
+
+    it "should know when a number is divisable by three and five" do
+     	fizzbuzz = Fizzbuzz.new
+        expect(fizzbuzz.is_divisable_by_three_and_five(15)).to eq true
+    end
+ 
+ 	it " should know when a number is not divisable by three and five" do
+ 		fizzbuzz = Fizzbuzz.new
+ 		expect(fizzbuzz.is_divisable_by_three_and_five(17)).to eq false
+ 	end
+
+    it "should return fizz when is divisable by three" do
+        fizzbuzz = Fizzbuzz.new
+        expect(fizzbuzz.find_answer(3)).to eq "fizz"
+    end
+
+    it "should not return fizz when its not divisable by three" do
+    	fizzbuzz = Fizzbuzz.new
+    	expect(fizzbuzz.find_answer(4)).not_to eq "fizz"
+    end
+
+
+end
+
